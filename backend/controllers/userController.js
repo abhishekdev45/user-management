396c69d94d5c5b4edc482f099a4e2e4e646caa82
abhishekdev45 <156638090+AbhishekWebdesys@users.noreply.github.com>
2024-07-13 00:37:24 +0530

    fixed a bug login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -62,12 +62,12 @@ const createUser = async (req, res) => {
     if (userExists) {
       return res.status(400).json({ message: "User already exists" });
     }
-
+    const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({
       firstName,
       lastName,
       email,
-      password,
+      password : hashedPassword,
       role,
     });
 
--- backend/models/User.js
+++ backend/models/User.js
@@ -29,14 +29,14 @@ const userSchema = new mongoose.Schema({
   resetPasswordExpire: Date,
 });
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
+// userSchema.pre('save', async function (next) {
+//   if (!this.isModified('password')) {
+//     return next();
+//   }
+//   const salt = await bcrypt.genSalt(10);
+//   this.password = await bcrypt.hash(this.password, salt);
+//   next();
+// });
 
 userSchema.methods.getResetPasswordToken = function () {
   const resetToken = crypto.randomBytes(20).toString('hex');
